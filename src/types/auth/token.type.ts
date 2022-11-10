417fda1af44c57181c7ae452513b04152155c321
Fernando Viana <fernando.vianadasilva@gmail.com>
2022-11-10 20:40:33 -0300

    docs: :memo: add docs to Token

--- src/types/auth/token.type.ts
+++ src/types/auth/token.type.ts
@@ -1,5 +1,16 @@
 export interface Token {
+  /**
+   * The token
+   */
   access_token: string;
+  /**
+   * The token type
+   * @example Bearer
+   */
   token_type: string;
+  /**
+   * The token expiration date in unix timestamp
+   * @example 1590000000
+   */
   expires_in: number;
 }
